8759d1d64a86f0d2a9b7eae52a0722beeb2cc24a
faqih1445 <alfaqih1445h@gmail.com>
2026-07-06 19:52:05 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: "/Undangan_Web_Digital/", // penting untuk GitHub Pages
+    base: "/Web_Undangan_Digital/", // penting untuk GitHub Pages
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
